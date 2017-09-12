9360649918a1715f1ad44fa6acbc643ac04e1b1a
前段课程学习 <123>
2017-09-12 11:27:45 +0800

    完善主页特效

--- liangcang/js/index.js
+++ liangcang/js/index.js
@@ -1,15 +1,44 @@
 window.onload = function(){
 	var index = 0;
+	var left = 0;
+	var timer;
 var oRightBtn = document.getElementById('carousel_rightBtn');
 var oLeftBtn = document.getElementById('carousel_leftBtn');
 var oImagesLists = document.getElementById('imagesList').getElementsByTagName('li');
 var oCirclesLists = document.getElementById('circles').getElementsByTagName('li');
+
+carousel();	
+		$("#carousel_leftBtn").mouseover(function(){
+			clearInterval(timer)
+		});
+		$("#carousel_leftBtn").mouseout(function(){
+			carousel();
+		})
+		$("#carousel_rightBtn").mouseover(function(){
+			clearInterval(timer)
+		});
+		$("#carousel_rightBtn").mouseout(function(){
+			carousel();
+		})
+		$("#imagesList").mouseover(function(){
+			clearInterval(timer)
+		});
+		$("#imagesList").mouseout(function(){
+			carousel();
+		})
+		$("#circles").mouseover(function(){
+			clearInterval(timer)
+		});
+		$("#circles").mouseout(function(){
+			carousel();
+		})
     oRightBtn.onclick = function(){
       index++;
       if (index >= oImagesLists.length) {
         index = 0;
       }
       move();
+      carousel()
     }
     oLeftBtn.onclick = function(){
       index--;
@@ -20,13 +49,20 @@ var oCirclesLists = document.getElementById('circles').getElementsByTagName('li'
     }
     for (var i = 0; i < oCirclesLists.length; i++) {
       (function(i){
-        oCirclesLists[i].onmouseover = function() {
+        oCirclesLists[i].onclick = function() {
           index = i;
           move();
         }
       })(i);
     }
     function move() {
+    	if (index < 0) {
+        index = oImagesLists.length-1;
+      }
+    	if (index > oImagesLists.length-1) {
+        index = 0;
+      }
+    	
       for (var i = 0; i < oImagesLists.length; i++) {
         oImagesLists[i].className = '';
       }
@@ -36,5 +72,22 @@ var oCirclesLists = document.getElementById('circles').getElementsByTagName('li'
         oCirclesLists[i].className = '';
       }
       oCirclesLists[index].className = 'current';
+      left = index * 1000;
+      	$("#imagesList ul").animate({
+      		"left":-left+"px"
+      	},600);
+    };
+    
+    function carousel(){
+    	clearInterval(timer);
+    timer =	setInterval(function(){
+      	index++;
+      	move();
+      	left = index * 1000;
+      	$("#imagesList ul").animate({
+      		"left":-left+"px"
+      	},600);
+      },3000);
     }
+    
 }
